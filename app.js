967b9c28da33ca61a62da99c55ea9840c6a9400b
alfoo <alfonsinacoiro@gmail.com>
2025-08-16 13:23:34 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let legado = parseFloat(prompt("Ingresa el legado total:"));
+
+let carlos = legado * (1/3);
+let jose = carlos *(4/3);
+let marta = (jose/2);
+
+console.log("Legado total: $", legado)
+console.log("Carlos recibe: $", carlos);
+console.log("Jose recibe: $", jose);
+console.log("Marta recibe: $", marta);
