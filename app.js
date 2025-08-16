945defbfc8df8e05556183fff10b530faa644f51
alfoo <alfonsinacoiro@gmail.com>
2025-08-16 13:44:50 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// Declarar variable
+let num=5;
+var ej="hola";
+let num2=10, num3=15.5, num4=-125;
+let car='5';
+let valor=false;
+let nombre="facundo";
+
+// Imprimir mensaje (console en consola) (alert como mensaje emergente) 
+//prompt para que el usuario ingrese algo (mensaje emergente tmb))
+console.log("hola, como estas", nombre)
+alert ("k ase", nombre)
+prompt ("k ase", nombre);
